dd6ae304f6ab49a8e28007c37abd6ad16a6a6ca4
Yuto Nishida <yuto@berkeley.edu>
2024-12-04 20:38:26 -0800

    Refer to dark-notify via pkgs

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -84,6 +84,16 @@ with final.pkgs.lib; let
     # (mkNvimPlugin inputs.wf-nvim "wf.nvim") # (example) keymap hints | https://github.com/Cassin01/wf.nvim
     # ^ bleeding-edge plugins from flake inputs
     which-key-nvim
+    # test
+    {
+      plugin = "test-the";
+      config = ''
+        lua << EOF
+          vim.g.exe_dark_notify = "${pkgs.dark-notify}/bin/dark-notify"
+        EOF
+      '';
+    }
+    # ^test
   ];
 
   extraPackages = with pkgs; [
