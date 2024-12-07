ffccfb3da9c2d28e24607092bcdf138441561fec
Yuto Nishida <yuto@berkeley.edu>
2024-12-06 21:48:43 -0800

    Add prelim custom which-key, and nvim-tree-lua

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -58,6 +58,7 @@ with final.pkgs.lib; let
     nvim-navic # Add LSP location to lualine | https://github.com/SmiteshP/nvim-navic
     statuscol-nvim # Status column | https://github.com/luukvbaal/statuscol.nvim/
     nvim-treesitter-context # nvim-treesitter-context
+    nvim-tree-lua # File explorer
     # ^ UI
     # themeing
     tokyonight-nvim
--- nvim/plugin/which-key.lua
+++ nvim/plugin/which-key.lua
@@ -1,3 +1,12 @@
+local nvim_tree_api = require('nvim-tree.api')
+
+
 require('which-key').setup {
-  preset = 'helix'
+  preset = 'helix',
+  spec = {
+    { "<leader><leader>e", function()
+      nvim_tree_api.tree.toggle()
+      --nvim_tree_api.tree.reload()
+    end, desc = "[nvim-tree] toggle [e]xplorer" },
+  },
 }
